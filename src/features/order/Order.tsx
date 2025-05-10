231f827b41e0286b4c741425e67b2b7c404a779c
HelenaIsh <elena-ishmatova@mail.ru>
2025-05-11 00:12:01 +0200

    feat: add order loading

--- src/features/order/Order.tsx
+++ src/features/order/Order.tsx
@@ -1,57 +1,17 @@
-// Test ID: IIDSAT
+// Test ID: IIDSAT, CQE92U
 
+import { useLoaderData, type LoaderFunctionArgs } from "react-router-dom";
+import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
 
-const order = {
-  status: "",
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
-
 function Order() {
-  const {
-    id,
-    status,
-    priority,
-    priorityPrice,
-    orderPrice,
-    estimatedDelivery,
-    cart,
-  } = order;
+  const { status, priority, priorityPrice, orderPrice, estimatedDelivery } =
+    useLoaderData();
+
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
@@ -81,6 +41,12 @@ function Order() {
       </div>
     </div>
   );
+}
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  if (!params.orderId) return;
+  const order = await getOrder(params.orderId);
+  return order;
 }
 
 export default Order;
