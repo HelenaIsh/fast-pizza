bef9a2849ecc6bcfc1f049cd8278fc5b65090fd4
HelenaIsh <elena-ishmatova@mail.ru>
2025-05-10 22:11:28 +0200

    feat: add routing

---
+++ src/utils/AppLayout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import { Header } from "./Header";
+
+export function AppLayout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Content</h1>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </>
+  );
+}
